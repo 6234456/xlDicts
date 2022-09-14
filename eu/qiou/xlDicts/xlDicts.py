7818314180ac1d844cd154380f0c70e3fbfce78c
6234456 <6234456@sina.com>
2022-09-14 20:13:52 +0200

    load and unload

--- eu/qiou/xlDicts/xlDicts.py
+++ eu/qiou/xlDicts/xlDicts.py
@@ -1,10 +1,18 @@
 from openpyxl import load_workbook
 from openpyxl.worksheet import worksheet
+from openpyxl.workbook import workbook
 
 class xlDicts:
-    def __init__(self, wb: str = "", data_only: bool = True):
+    def __init__(self, wb: str = "", data_only: bool = True, auto_save: bool = False):
         self.file = wb
-        self.wb = load_workbook(wb,  data_only=data_only)
+        self.wb: workbook = load_workbook(wb,  data_only=data_only)
+        self.autoSave = auto_save
+        self.data = dict()
+        self.struct = dict()
+        self.structuredData = dict()
+
+    def __del__(self):
+        self.wb.close()
 
     def __getSht(self, sht: str = "") -> worksheet:
         if len(sht) > 0:
@@ -12,7 +20,7 @@ class xlDicts:
         else:
             return self.wb.active
 
-    def load(self, sht: str = "", keyCol: int = 1, valCol = 2, startRow : int = 1, endRow: int = None, ignoreNullVal: bool = True, setNullValTo = 0, reversed:bool = False):
+    def load(self, sht: str = "", keyCol: int = 1, valCol = 2, startRow : int = 1, endRow: int = None, ignoreNullVal: bool = True, setNullValTo = 0, reversed:bool = False, asFormula:bool = False):
         """
                    load the data on the spreadsheet to dict
                    @param
@@ -28,8 +36,6 @@ class xlDicts:
                            setNullValTo    set the None value to the given value, valid only when ignoreNullVal is False
                            reversed        read from top to bottom. the duplicated value will be replaced by the one below
                """
-
-
         s = self.__getSht(sht)
         end = endRow or s.max_row
 
@@ -43,41 +49,162 @@ class xlDicts:
 
         filterCol = [i - min(valCol) for i in valCol]
         k = list(list(s.iter_cols(keyCol, keyCol, startRow, end, True))[0])
-        v = [[i[j] for j in filterCol] for i in s.iter_rows(startRow, endRow, min(valCol), max(valCol), True)]
+        if asFormula:
+            t = s.title
+            v = [[i[j] for j in filterCol] for i in
+                [
+                [f"'{t}'!{s.cell(y, x).column_letter}{y}" for x in range(min(valCol), max(valCol) + 1)]
+                    for y in range(startRow, end + 1)]
+                ]
+        else:
+            v = [[i[j] for j in filterCol] for i in s.iter_rows(startRow, end, min(valCol), max(valCol), True)]
 
         if reversed:
             k.reverse()
             v.reverse()
 
-        self.d = {k: v for k, v in dict(zip(k, v)).items() if not k == None}
+        self.data = {k: v for k, v in dict(zip(k, v)).items() if not k is None}
 
-        if ignoreNullVal:
-            self.d = {k: [i or setNullValTo for i in v] for k, v in self.d.items() if any(v)}
+        if not asFormula and ignoreNullVal:
+            self.data = {k: [i or setNullValTo for i in v] for k, v in self.data.items() if any(v)}
 
         if len(valCol) == 1:
-            self.d = {k: v[0] for k, v in self.d.items()}
+            self.data = {k: v[0] for k, v in self.data.items()}
 
-    def fromDict(self, d:dict):
-        self.d = d
+        return self
 
-    def unload(self, sht: str = "", keyCol: int = 1, startCol = 2, startRow : int = 1, endRow: int = None):
-        if self.d and len(self.d):
-            s = self.__getSht(sht)
-            l = list(list(self.d.values())[0])
+    def loadStruct(self, sht: str = "", keyCol: int = 1, valCol: int = 2):
+        s = self.__getSht(sht)
+        d = zip(list(list(s.iter_cols(keyCol, keyCol, None, None, True))[0]),
+                list(list(s.iter_cols(valCol, valCol, None, None, True))[0]))
+        acc = dict()
+        tmp = list()
+        lastK = None
+        for k, v in d:
+            if k is not None and v is None:
+                if lastK is not None:
+                    acc[lastK] = tmp
+                lastK = k
+                tmp = []
+            elif k is None and v is not None:
+                tmp.append(v)
+
+        acc[lastK] = tmp
+        self.struct = acc
+
+        return self
+
+    def feed(self, data=None):
+        if not type(data) is dict:
+            if data is None:
+                data = self.data
+            elif type(data) is xlDicts:
+                data = data.data
+            else:
+                raise TypeError("the type of {0} is invalid!\n xlDicts or dict is required.")
+
+        self.structuredData = {k: {k0: data[k0] for k0 in v if k0 in data} for k, v in self.struct.items()}
+        return self
+
+    def aggregate(self, func=lambda dic: sum(dic.values())):
+        self.data = {k: func(v) for k, v in self.structuredData.items()}
+        return self
+
+    def fromDict(self, d: dict):
+        self.data = d
+        return self
+
+    def unload(self, sht: str = "", keyCol: int = 1, startCol: int = 2, startRow: int = 1, endRow: int = None):
+        if len(self.data) > 0:
+            s: worksheet = self.__getSht(sht)
             end = endRow or s.max_row
-            ks = list(self.d.keys())
+            ks = list(self.data.keys())
             for r in range(startRow, end + 1):
                 k = s.cell(column=keyCol, row=r).value
                 if k in ks:
-                    tmp = self.d[k]
+                    if not type(self.data[k]) is list:
+                        tmp = [self.data[k]]
+                    else:
+                        tmp = self.data[k]
+
                     cnt = 0
-                    for c in range(startCol, startCol + len(l)):
+                    for c in range(startCol, startCol + len(tmp)):
                         s.cell(column=c, row=r, value=tmp[cnt])
                         cnt = cnt + 1
 
-            self.wb.save(self.file)
+            if self.autoSave:
+                self.save()
 
+        return self
 
+    def dump(self, sht: str = "", topRow: int = 1, leftCol: int = 1):
+        if len(self.data) > 0:
+            s: worksheet = self.__getSht(sht)
+            r = topRow
+
+            for k, v in self.data.items():
+                if not type(v) is list:
+                    l = [v]
+                else:
+                    l = v
+
+                cnt = 0
+                s.cell(column=leftCol, row=r, value=k)
+                for c in range(leftCol+1, leftCol + 1 + len(l)):
+                    s.cell(column=c, row=r, value=l[cnt])
+                    cnt = cnt + 1
+
+                r = r + 1
+
+            if self.autoSave:
+                self.save()
+
+        return self
+
+    def dumpStructuredData(self, sht: str = "", topRow: int = 1, leftCol: int = 1):
+        if len(self.structuredData) > 0:
+            s: worksheet = self.__getSht(sht)
+
+            r = topRow
+
+            for k0, v0 in self.structuredData.items():
+                s.cell(column=leftCol, row=r, value=k0)
+                r = r + 1
+                for k, v in v0.items():
+                    s.cell(column=leftCol + 1, row=r, value=k)
+                    cnt = 1
+                    for r0 in v:
+                        s.cell(column=leftCol + 1 + cnt, row=r, value=r0)
+                        cnt = cnt + 1
+
+                    r = r + 1
+
+            if self.autoSave:
+                self.save()
+
+        return self
+
+    def dumpStruct(self, sht: str = "", topRow: int = 1, leftCol: int = 1):
+        if len(self.struct) > 0:
+            s: worksheet = self.__getSht(sht)
+
+            r = topRow
+
+            for k, v in self.struct:
+                s.cell(column=leftCol, row=r, value=k)
+                r = r + 1
+                for r0 in v:
+                    s.cell(column=leftCol+1, row=r, value=r0)
+                    r = r + 1
+
+            if self.autoSave:
+                self.save()
+
+        return self
+
+    def save(self):
+        self.wb.save(self.file)
+        return self
 
 
 
--- test.py
+++ test.py
@@ -4,9 +4,14 @@ from eu.qiou.xlDicts.xlDicts import xlDicts
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         d = xlDicts("123.xlsx")
-        d.load("1",1,[3, 2], reversed= False)
-        print(d.d)
-        d.unload("123", endRow=3)
+        d.load("1",1,[3, 2], reversed= False, asFormula=True).unload("123")
+        print(d.data)
+        d.loadStruct("mapping").feed()
+        print(d.struct)
+        print(d.structuredData)
+        d.dumpStructuredData("123", 1, 20)
+        d.aggregate(lambda dic: "=" +("+".join([v[0] for v in dic.values()]))).dump("123", 20, 20).save()
+        # d.aggregate(lambda dic: sum(([v[0] for v in dic.values()]))).dump("123", 20, 20).save()
 
 
 if __name__ == '__main__':
